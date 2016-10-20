3fc17742095de54d552c18104297fde7d72ae7b3
yyoshiki41 <yyoshiki41@gmail.com>
2016-10-20 23:44:39 +0900

    mkdir cmd

---
+++ cmd/radigo/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/yyoshiki41/radigo"
+)
+
+var (
+	// ErrUsage is returned when a usage message was printed and the process
+	// should simply exit with an error.
+	ErrUsage = errors.New("usage")
+
+	// ErrUnknownCommand is returned when a CLI command is not specified.
+	ErrUnknownCommand = errors.New("unknown command")
+)
+
+func main() {
+	if len(os.Args) == 0 {
+		fmt.Fprintln(os.Stderr, usage())
+	}
+
+	err := run(os.Args[1:]...)
+	if err == ErrUsage {
+		os.Exit(2)
+	}
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
+
+func run(args ...string) error {
+	// print the version
+	if args[0] == "version" || args[0] == "-v" {
+		fmt.Fprintln(os.Stderr, radigo.Version())
+		return nil
+	}
+
+	// require a command at the beginning
+	if strings.HasPrefix(args[0], "-") {
+		fmt.Fprintln(os.Stderr, usage())
+		return ErrUsage
+	}
+
+	// Execute command.
+	switch args[0] {
+	case "help":
+		fmt.Fprintln(os.Stderr, usage())
+		return ErrUsage
+	case "rec":
+		return nil
+	default:
+		return ErrUnknownCommand
+	}
+}
+
+func usage() string {
+	return strings.TrimLeft(`
+radigo is a tool for recording radiko.
+Usage:
+    radigo command [arguments]
+
+The commands are:
+    help        Print this screen.
+    version     Print the version.
+    rec         Record a radiko program.
+    search      Search a radiko program.
+
+Use "radigo [command] -h" for more information about a command.
+`, "\n")
+}
---
+++ radigo.go
@@ -0,0 +1,13 @@
+package radigo
+
+import "fmt"
+
+const version = "0.0.1"
+
+func Version() string {
+	return version
+}
+
+func Run() {
+	fmt.Println("vim-go")
+}
