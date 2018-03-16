d27c80a20873c85052b315262373fb731061c55e
yyoshiki41 <yyoshiki41@gmail.com>
2018-03-16 19:55:29 +0900

    test(main): cosme

--- main_test.go
+++ main_test.go
@@ -3,7 +3,6 @@ package radigo
 import (
 	"flag"
 	"os"
-	"strings"
 	"testing"
 )
 
@@ -12,20 +11,4 @@ func TestMain(m *testing.M) {
 
 	code := m.Run()
 	os.Exit(code)
-}
-
-func TestAreaID(t *testing.T) {
-	if currentAreaID == "" {
-		t.Errorf("currentAreaID is empty")
-	}
-
-	if !(strings.HasPrefix(currentAreaID, "JP") || currentAreaID == "OUT") {
-		t.Errorf("Invalid currentAreaID: %s", currentAreaID)
-	}
-}
-
-func TestLocation(t *testing.T) {
-	if actual := location.String(); actual != tz {
-		t.Errorf("location is expected %s, but %s", tz, actual)
-	}
 }
--- radigo_test.go
+++ radigo_test.go
@@ -1,6 +1,25 @@
 package radigo
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
+
+func TestAreaID(t *testing.T) {
+	if currentAreaID == "" {
+		t.Errorf("currentAreaID is empty")
+	}
+
+	if !(strings.HasPrefix(currentAreaID, "JP") || currentAreaID == "OUT") {
+		t.Errorf("Invalid currentAreaID: %s", currentAreaID)
+	}
+}
+
+func TestLocation(t *testing.T) {
+	if actual := location.String(); actual != tz {
+		t.Errorf("location is expected %s, but %s", tz, actual)
+	}
+}
 
 func TestVersion(t *testing.T) {
 	if Version() == "" {
